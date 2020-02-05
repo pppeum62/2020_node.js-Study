3f95f33934a6a55669e093b9d75d1781aad62dfb
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-02-05 19:56:34 +0900

    조건문
    if, else
    switch-case

---
+++ javascript-basics/if-else.js
@@ -0,0 +1,12 @@
+var a = 3;
+var result = '';
+
+if(a > 2) {
+    result = "a is greater than 2";
+} else if(a == 2) {
+    result = "a is 2";
+} else {
+    result = "a is smaller than 2";
+}
+
+console.log(result);    // "a is greater than 2"
---
+++ javascript-basics/switch-case.js
@@ -0,0 +1,16 @@
+var a = 1;
+var result = '';
+
+switch (a) {
+    case 1:
+        result = "Number 1";
+        break;
+    case 2:
+        result = "Number 2";
+        break;
+    default:
+        result = "I don't know what number";
+        break;
+}
+
+console.log(result);    // "Number 1"
