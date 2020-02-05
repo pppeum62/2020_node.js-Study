d32e688135da3af6a1c8f6202be73119b96b68c0
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-02-05 19:18:29 +0900

    javascript 기본 문법

---
+++ 0205/test_01.js
@@ -0,0 +1,16 @@
+var a = 1;
+b = 2;
+
+console.log("a : " + a);
+console.log("b : " + b);
+
+var c = 3, d = 10;
+var str1 = "Hello";
+var str2 = "World!";
+var str3 = null;
+
+console.log("c : " + c);
+console.log("d : " + d);
+console.log(str1);
+console.log(str2);
+console.log(str3);
