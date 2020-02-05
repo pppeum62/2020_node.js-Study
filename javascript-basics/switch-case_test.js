f72807dc1c3616c83fe897821faf507f7dd82336
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-02-05 20:27:39 +0900

    조건문 실습 - 자판기

---
+++ javascript-basics/switch-case_test.js
@@ -0,0 +1,29 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+rl.on("line", function(line) {
+	// if문과 switch문 이용
+	// 입력값 출력은 아래와 같이 사용
+	// console.log(line);
+	
+	switch(line) {
+		case "200":
+			console.log("웰치스");
+			break;
+		case "300":
+			console.log("코코팜");
+			break;
+		case "500":
+			console.log("아이스티");
+			break;
+		default:
+			console.log(line);
+			break;
+	}
+	rl.close();
+}).on("close", function() {
+	process.exit();
+});
