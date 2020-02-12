28cbff596fb5f05dd2a9b784bec2ab3c0e998f5f
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-02-13 00:20:38 +0900

    bingo-game

--- node.js/bingo-game/server.js
+++ node.js/bingo-game/server.js
@@ -1,71 +1,71 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http').Server(app); 
+var io = require('socket.io')(http);    
 var path = require('path');
 
-app.set('view', path.join(__dirname, 'view'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('main', { title: '온라인 빙고 게임', username: req.query.username});
+    res.render('main', { title: '온라인 빙고 게임', username: req.query.username });
 });
 
-var users = { };
+var users = {};
 var user_count = 0;
 var turn_count = 0;
 
-io.on('connection', function(socket) {
-    console.log('user connected : ', socket.id);
-
-    socket.on('join', function(data) {
-        var username = data.username;
-        socket.username = username;
-
-        users[user_count] = { };
-        users[user_count].id = socket.id;
-        users[user_count].name = username;
-        users[user_count].turn = false;
-        user_count++;
-
-        io.emit('update_users', users, user_count);
-    });
-
-    socket.on('game_start', function(data) {
-        socket.broadcast.emit("game_started", data);
-        users[turn_count].turn = true;
-
-        io.emit('updata_uers', users);
-    });
-
-    socket.on('select', function(data) {
-        socket.broadcast.emit("check_number", data);
-
-        uesrs[turn_count].turn = false;
-        turn_count++;
-
-        if(turn_count >= user_count) {
-            turn_count = 0;
-        }
-        user[turn_count].turn = false;
-
-        io.sockets.emit('updata_users', users);
-    });
-
-    socket.on('disconnect', function() {
-        console.log('user disconnected : ', socket.id, socket.username);
-        for(var i = 0; i < user_count; i++) {
-            if(users[i].id == socket.id){
-                delete users[i];
-            }
-        }
-
-        user_count--;
-        io.emit('update_users', users, user_count);
-    });
+io.on('connection', function(socket){ 
+	
+	console.log('user connected : ', socket.id);
+	
+	socket.on('join', function (data) {
+		var username = data.username;
+		socket.username = username;
+		
+		users[user_count] = {};
+		users[user_count].id = socket.id;
+		users[user_count].name = username;
+		users[user_count].turn = false;
+		user_count++;
+		
+		io.emit('update_users', users, user_count);
+	});
+	
+	socket.on('game_start', function (data) {
+		socket.broadcast.emit("game_started", data);
+		users[turn_count].turn = true;
+		
+		io.emit('update_users', users);
+	});
+	
+	socket.on('select', function (data) {
+		socket.broadcast.emit("check_number", data);
+		
+		users[turn_count].turn = false;
+		turn_count++;
+		
+		if(turn_count >= user_count) {
+			turn_count = 0;
+		}
+		users[turn_count].turn = true;
+		
+		io.sockets.emit('update_users', users);
+	});
+	
+	socket.on('disconnect', function() {
+		console.log('user disconnected : ', socket.id, socket.username);
+		for(var i=0; i<user_count; i++){
+			if(users[i].id == socket.id)
+				delete users[i];
+		}	
+		
+		user_count--;
+		io.emit('update_users', users, user_count);
+	});
 });
 
-http.listen(3000, function() {
-    console.log('server on!');
+http.listen(3000, function(){ 
+	console.log('server on!');
 });
