7226f568e8e6b6b917726a5d7771676b53c51e22
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-02-14 22:54:56 +0900

    mongoDB connection

---
+++ node.js/mongoDB/connection02.js
@@ -0,0 +1,3 @@
+var mongoose = require('mongoose');
+var connection1 = mongoose.createConnection('mongodb://localhost/mydb1');
+var connection2 = mongoose.createConnection('mongodb://localhost/mydb2');
